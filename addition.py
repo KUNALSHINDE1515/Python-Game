f2c2557640f147a0106467f21700ace1c28257f0
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2023-09-14 12:38:13 +0530

    addition

---
+++ addition.py
@@ -0,0 +1,18 @@
+# addition multi sub div
+a=70
+b=50
+# add
+c=a+b
+print("Addition is :",c)
+
+# sub
+c=a-b
+print("Addition is :",c)
+# multi
+c=a*b
+print("Addition is :",c)
+
+#div 
+c=a/b
+print("Addition is :",c)
+
