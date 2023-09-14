674cf7c6626bc7207bf721fc405d1ee6c172e873
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2023-09-14 12:22:12 +0530

    set

---
+++ set.py
@@ -0,0 +1,19 @@
+# Set
+var={10,20,'kunal',56.25,True,7,"kunal",100}
+var2={10,20,'kunal',56.25,True,"india",False}
+
+# var.add("shinde")
+# var.update("Deshmukh",)
+
+
+# print(var.pop())
+# var.remove("kunal")
+
+# var.clear()
+
+# print(var.union(var2))
+# print(var.intersection(var2))
+
+# print(var.difference(var2))
+
+print(var.symmetric_difference(var2))
