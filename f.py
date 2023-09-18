f584cfbf2c7c517467c04092f13fbdf550bb82ae
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2023-09-18 13:53:27 +0530

    update

---
+++ f.py
@@ -0,0 +1,59 @@
+# # # # # # here print and len is in-built funcyion
+# # # # # # print ("this is a print finction")
+
+# # # # # # print (lenn("Something"))
+ 
+# # # # # def kunal() :
+# # # # #     a=5
+# # # # #     b=6
+# # # # #     c=a+b
+# # # # #     print(c)
+
+# # # # # kunal() 
+
+
+# # # # def calulator(a,b):
+# # # #     print("addition :",a+b)
+# # # #     print("substraction :",a-b)
+# # # #     print("multi :",a*b)
+# # # #     print("div :",a/b)
+
+# # # # calulator(8,9)
+# # # # calulator(10,93)
+# # # # calulator(8,6)
+# # # # calulator(8,2)
+
+# # # # positional argument
+# # # def great(name,lastname):
+# # #     print("good morning",name,lastname)
+
+# # # msg=great("kunal","Shinde")
+# # # print(msg)
+
+
+# # # keywords argumnet
+
+# # # def great(name,lastname):
+# # #     print("good morning",name,lastname)
+
+# # # msg=great(lastname="Shinde",name="kunal")
+# # # print(msg)
+
+# # # default argument
+# # def great(name="guest"):
+# #     print("good morning",name)
+
+# # msg=great("kunal")
+# # print(msg)
+
+
+# # variable lenght argumnet
+# def test(*args):
+#     print(args)
+#     print(len(args))
+#     print(type(args))
+
+# test()
+# test("india","america","china")
+
+
