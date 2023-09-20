99c8a724f37f5c1b6d39c1165997e611d8ec9fae
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2023-09-20 15:06:46 +0530

    update

---
+++ exception-handling.py
@@ -0,0 +1,9 @@
+a=int(input("Enter first Number: "))
+b=int(input("Enter secound Number: "))
+try:
+    c=a/b
+    print("Result :",c)
+except:
+    print("Cannot divide zero....!")
+else:
+    print("kunal")
