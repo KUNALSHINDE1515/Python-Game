72a14111fe2a75ff23d882b81bdd1485152bb615
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2023-09-26 14:11:24 +0530

    OOPS concept

---
+++ oop.py
@@ -0,0 +1,22 @@
+# # class A:
+# #      def __init__(self):
+# #       age=20
+# #       print(age)
+# # obj=A()
+# # obj=A()
+# # obj=A()
+
+
+# class A:
+#     "learning phases.."
+#     age=10
+#     print(age)
+# obj=A()
+# print(obj.__doc__)
+
+class A:
+    
+    def fun(self,age,name,add):
+         print(age," ",name," ",add)
+obj=A()
+obj.fun(10,"kunal","Pune")
