2e0b25abc2ea9b33f84f75ebfbe92da645341e92
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2023-09-30 16:39:31 +0530

    inhe

---
+++ inheritance.py
@@ -0,0 +1,12 @@
+                        # Python Inheritance  #
+class Father:
+    def Lands(self):
+        print("Having 50 Ekar lands")
+
+class Son(Father):
+     def Money(self):
+         print("Having 10 lakhs Money")
+
+s=Son()
+s.Lands()
+s.Money()
