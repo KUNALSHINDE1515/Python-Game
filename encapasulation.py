0a0d2e1254856c4ec64d6fab081a6bd60717bd09
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2023-10-06 16:20:58 +0530

    encap Ex

---
+++ encapasulation.py
@@ -0,0 +1,15 @@
+                #   Python encapasulation
+
+class A:
+    _a=10  #Protected
+    __b=20 #Private
+
+    def Show(self):
+        print("a= ",self._a)
+        print("b= ",self.__b)
+
+obj=A()
+obj.Show()
+print("Outside of class ",obj._a)
+
+print("Outside of class ",obj.__b)
